4fc615361090503e7072fab33e1bcbc9146409d7
HovhannesHovhanisyan97 <60502077+HovhannesHovhanisyan97@users.noreply.github.com>
2020-05-29 13:21:16 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 
 
 let input = document.querySelector("#input");
-let btn = document.querySelector(".button")
-let box = document.querySelector("#activ")
-
-let active = document.getElementById('activ')
+let btn = document.querySelector(".button");
+let box = document.querySelector("#activ");
+let arr = [];
+let active = document.getElementById('activ');
+let search = document.getElementById('search')
 
 function createElem (text){
-   let newElem = document.createElement("li");
-   let deleteElem = document.createElement("span");
+    let newElem = document.createElement("li");
+    let deleteElem = document.createElement("span");
+    
    deleteElem.className = 'delete_element'
    deleteElem.textContent = "delete"
    newElem.className = 'newElem_creation'
-   newElem.textContent = text
-   newElem.appendChild(deleteElem);
+   if(!(arr.includes(text))){
+    newElem.textContent = text
+    newElem.appendChild(deleteElem);
+    box.appendChild(newElem)
+    arr.push(text)
+   }else{
+       alert('Mentioned note exesist')
+   }
+   
      deleteElem.addEventListener('click', function(){
-         
+         let txt = newElem.innerText
+         let txt1 = txt.slice(0,txt.length-7)
+         if(confirm('Are you sure you want to delete')){
+         let index = arr.indexOf(txt1)
+         arr.splice(index,1)
          box.removeChild(newElem)
          input.value = ''
-         
+         }else{
+            event.stopPropagation()
+            alert('Rejected')
+         }
      })
      
       newElem.addEventListener("click" , function(){
@@ -34,7 +50,7 @@ function createElem (text){
           }
       })
 
-   box.appendChild(newElem)
+   
 
 };
 
@@ -42,6 +58,7 @@ btn.addEventListener("click", function(e){
    e.preventDefault();
    if(!(input.value === "")){
        createElem(input.value)
+       input.value = ''
    }else{
        alert('empty')
    }
@@ -60,7 +77,20 @@ input1.addEventListener('keyup', function(e){
     }
 })
 
-btn.addEventListener('click',function(e){
-    e.preventDefault();
-    input.value = ''
-})
+
+
+function searchFn(){
+    let allLiItems = active.getElementsByClassName('newElem_creation')
+    for(key of allLiItems){
+        let item1 = key.textContent;
+        let text1 = item1.slice(0,item1.length-6)
+       if(text1.includes(search.value)){
+           key.classList.remove('disp')
+       }else{
+           key.classList.add('disp')
+       }
+    }
+}
+
+
+search.addEventListener('input',searchFn)
